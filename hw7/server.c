ba867a2281f73652612e6e69dae2e784c1393517
MrTsepa <tsepa.stas@gmail.com>
2015-10-28 20:50:50 +0300

    server with N threads

--- hw7/server.c
+++ hw7/server.c
@@ -8,7 +8,7 @@
 #include <pthread.h>
 #include <sys/sem.h>
 
-#define SLEEP_TIME 20
+#define SLEEP_TIME 10
 #define MAX_THREAD_NUMBER 2
 
 int msqid;
@@ -32,33 +32,32 @@ struct SndMsg {
 	} info;
 };
 
-void *thread_func(void *RcvBuf)
-{
-	printf("Executing task of pid %d. Number of free threads = \n", ((struct RcvMsg*)RcvBuf)->info.pid);
+void *thread_func(void *ptrrcvBuf)
+{	
+	struct RcvMsg rcvBuf = *((struct RcvMsg*)ptrrcvBuf);
+	printf("Executing task of pid %d.\n", rcvBuf.info.pid);
 
-	struct SndMsg SndBuf;
-	int snd_len = sizeof(SndBuf.info);
+	struct SndMsg sndBuf;
+	int snd_len = sizeof(sndBuf.info);
 
-	SndBuf.mtype = ((struct RcvMsg*)RcvBuf)->info.pid;
-	SndBuf.info.res = ((struct RcvMsg*)RcvBuf)->info.a * ((struct RcvMsg*)RcvBuf)->info.b;
+	sndBuf.mtype = rcvBuf.info.pid;
+	sndBuf.info.res = rcvBuf.info.a * rcvBuf.info.b;
 	sleep(SLEEP_TIME);
 
-	if (msgsnd(msqid, (struct SndMsg *) &SndBuf, snd_len, 0) < 0) {
+	if (msgsnd(msqid, (struct SndMsg *) &sndBuf, snd_len, 0) < 0) {
 		printf("Can\'t send message to queue\n");
 		msgctl(msqid, IPC_RMID, (struct msqid_ds*)NULL);
 		exit(-1);
 	}
 	else {
 		printf("Message sent to pid %lu. Res = %d\n",
-				SndBuf.mtype, SndBuf.info.res);
+				sndBuf.mtype, sndBuf.info.res);
 	}
 
 	semaphore.sem_flg = 0;
 	semaphore.sem_num = 0;
 	semaphore.sem_op = 1;
 	semop(semid, &semaphore, 1);
-
-	printf("	Waiting to recieve...\n");
 
 	return NULL;
 }
@@ -69,10 +68,8 @@ int main()
 	key_t key, key_s;
 
 	int i;
-	struct SndMsg SndBuf;
-	struct RcvMsg RcvBuf;
-	int rcv_len = sizeof(RcvBuf.info);
-	int snd_len = sizeof(SndBuf.info);
+	struct RcvMsg rcvBuf;
+	int rcv_len = sizeof(rcvBuf.info);
 
 	if ((key = ftok(pathname, 0)) < 0) {
 		printf("Can\'t generate key\n");
@@ -92,34 +89,41 @@ int main()
 		exit(-1);
 	}
 
-	semaphore.sem_flg = 0;
-	semaphore.sem_num = 0;
-	semaphore.sem_op  = MAX_THREAD_NUMBER;
-	semop(semid, &semaphore, 1);
+	int semval = semctl(semid, 0, GETVAL);	//
+	semaphore.sem_flg = 0;			// обнуление семафора	
+	semaphore.sem_num = 0;			// перед началом работы
+	semaphore.sem_op  = -1*semval;		//
+	semop(semid, &semaphore, 1);		//
+							
+	semaphore.sem_flg = 0;				
+	semaphore.sem_num = 0;				
+	semaphore.sem_op  = MAX_THREAD_NUMBER;		
+	semop(semid, &semaphore, 1);			
 
 	while(1)
 	{
-		printf("	Waiting to recieve...\n");
+		semaphore.sem_flg = 0;
+		semaphore.sem_num = 0;
+		semaphore.sem_op = -1;
+		semop(semid, &semaphore, 1);
 
-		if (msgrcv(msqid, (struct RcvMsg*)&RcvBuf, rcv_len, 1, 0) < 0) {
+		struct RcvMsg rcvBuf;
+		int rcv_len = sizeof(rcvBuf.info);
+
+		if (msgrcv(msqid, (struct RcvMsg*)&rcvBuf, rcv_len, 1, 0) < 0) {
 			printf("Can\'t recieve message from queue\n");
 			msgctl(msqid, IPC_RMID, (struct msqid_ds*)NULL);
 			exit(-1);
 		}
 		else {
 			printf("Message recieved from pid %d. info = %d, %d\n",
-				       RcvBuf.info.pid, RcvBuf.info.a,
-				       RcvBuf.info.b);
+				       rcvBuf.info.pid, rcvBuf.info.a,
+				       rcvBuf.info.b);
 		}
 
 		pthread_t thread;
 
-		semaphore.sem_flg = 0;
-		semaphore.sem_num = 0;
-		semaphore.sem_op = -1;
-		semop(semid, &semaphore, 1);
-
-		if (pthread_create(&thread, NULL, thread_func, (void*)&RcvBuf) != 0) {
+		if (pthread_create(&thread, NULL, thread_func, (void*)&rcvBuf) != 0) {
 			return EXIT_FAILURE;
 		}
 	}
