1902e40931cce2c9c57234fad902a368ad714c57
MrTsepa <tsepa.stas@gmail.com>
2015-11-10 18:00:40 +0300

    fixed hw10

--- hw10/a.c
+++ hw10/a.c
@@ -7,42 +7,22 @@
 
 pid_t ppid, chpid;
 
-/*
- * У вас переменная ppid не инициализирована. В debug режиме ей присваивается значение 0.
- * Написав kill(0, ...) вы отправляете сигнал всем процессам в рабочей группе (мы пропустили это на семинаре, сказав, что общаются только родственные процессы).
- * Т.е. с большой вероятностью процесс сам себе что-то отправлял.
- */
-
-char * str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+char * str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 char ** str_res_ptr;
 
 char cur_char = 0;
 int pos_in_char = 0;
 int pos_in_str = 0;
 
-/*
- * Зачем вам эта ф-я?
- * Можно ведь написать 1 << p.
- */
-char pwr_of_2(int p)
-{
-	int i;
-	int res = 1;
-	for (i = 0; i < p; i++) {
-		res *= 2;
-	}
-	return res;
-}
-
 void handler(int nsig) {
 	char* str_res = *str_res_ptr;
 	int t;
-	if (nsig == 30)
+	if (nsig == SIGUSR1)
 		t = 0;
-	if (nsig == 31)
+	if (nsig == SIGUSR2)
 		t = 1;
 	if (pos_in_char < 8) {
-		cur_char += t * pwr_of_2(pos_in_char);
+		cur_char += t * (1 << pos_in_char);
 		pos_in_char++;
 	}
 	if (pos_in_char == 8) {
@@ -54,22 +34,32 @@ void handler(int nsig) {
 	if (pos_in_str == strlen(str)) {
 		str_res[pos_in_str] = '\0';
 		printf("%s\n", str_res);
+		kill(chpid, SIGKILL);
+		exit(0);
 	}
+	kill(chpid, SIGINT);
 }
+
+int a;
+
+void ch_handler(int nsig) 
+{}
 
 int main()
 {
 	str_res_ptr = (char**)malloc(sizeof(char*));
-	*str_res_ptr = (char*)malloc(sizeof(char)*strlen(str));
+	*str_res_ptr = (char*)malloc(sizeof(char)*(strlen(str)+1));
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	signal(SIGINT, ch_handler);
 	pid_t pid = fork();
-/*
- * Зачем разводить магические числа на ровном месте. Даже в задании написано, что сигналы
- * называются SIGUSR1, SIGUSR2. Зачем вам тогда 30 и 31.
- */
-	signal(30, handler);
-	signal(31, handler);
   
 	if (pid != 0) {
+		chpid = pid;
+		while(1) {;}
+	}
+	if (pid == 0) {
+		ppid = getppid();
 		int i;
 		for (i = 0; i < strlen(str); i++) {
 			int j;
@@ -77,12 +67,13 @@ int main()
 			for (j = 0; j < sizeof(char) * 8; j++) {
 				int flag = c % 2;
 				if (flag == 0) {
-					kill(ppid, 30);
+					kill(ppid, SIGUSR1);
 				}
 				if (flag == 1) {
-					kill(ppid, 31);
+					kill(ppid, SIGUSR2);
 				}
 				c = c >> 1;
+				pause();
 			}
 		}
 	}
